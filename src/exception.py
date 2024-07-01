1898514afeead8f44586311a48b54ee1ab8f08fa
abhishektarun09 <abhishek.tarun09@gmail.com>
2024-07-01 19:24:24 +0200

    Data ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,11 @@
 import sys
+
+from os.path import dirname, abspath
+
+# Add the src directory to the sys.path
+sys.path.append(dirname(dirname(abspath(__file__))))
+
+
 from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
@@ -8,7 +15,7 @@ def error_message_detail(error, error_detail:sys):
     
     return error_message
     
-class CustomExeption(Exception):
+class CustomException(Exception):
     def __init__(self, error_message, error_detail:sys):
         super().__init__(error_message)
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
